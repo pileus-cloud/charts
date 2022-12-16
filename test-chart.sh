f6f089e7615edf32114ff0aa342314ea82b86701
Дар'я Харлан <dadaka.x@gmail.com>
2022-12-16 17:47:54 +0100

    add kube-prometheus-stack to test PrometheusRule

--- test-chart.sh
+++ test-chart.sh
@@ -3,6 +3,11 @@ CHART_NAME=$1
 sudo microk8s helm3 upgrade -i --atomic $CHART_NAME helm-chart-sources/$CHART_NAME
 
 export STATUS=$(sudo microk8s kubectl get po | grep $CHART_NAME | awk '{print $3}')
+if [ $STATUS != "" ]
+then
+  exit 1
+fi
+
 while [ $STATUS == "ContainerCreating" ]
 do
   export STATUS=$(sudo microk8s kubectl get po | grep $CHART_NAME | awk '{print $3}')
