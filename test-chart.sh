2d3b18a5efd98af4598842bd506754d2f42ee69f
Дар'я Харлан <dadaka.x@gmail.com>
2022-12-16 17:30:43 +0100

    test chat

---
+++ test-chart.sh
@@ -0,0 +1,17 @@
+CHART_NAME=$1
+
+sudo microk8s helm3 upgrade -i --atomic $CHART_NAME helm-chart-sources/$CHART_NAME
+
+export STATUS=$(sudo microk8s kubectl get po | grep $CHART_NAME | awk '{print $3}')
+while [ $STATUS == "ContainerCreating" ]
+do
+  export STATUS=$(sudo microk8s kubectl get po | grep $CHART_NAME | awk '{print $3}')
+  sleep 1
+done
+
+sudo microk8s kubectl get po
+
+if [ $STATUS != "Running" ]
+then
+  exit 1
+fi
