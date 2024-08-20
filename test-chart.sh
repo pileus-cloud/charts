0ca6fce94678e08b17280b716eea0af713666dbc
Maksym Kryukov <max.krukov89@gmail.com>
2024-08-20 19:33:16 +0300

    Update test-chart.sh

--- test-chart.sh
+++ test-chart.sh
@@ -5,9 +5,10 @@ helm3() {
    sudo microk8s helm3 upgrade -i --atomic $CHART_NAME helm-chart-sources/$CHART_NAME --set workload=Deployment --set kubePrometheusStack.enabled=true
 }
 
-helm3 &
+(helm3 &)
 
-while (pgrep microk8s.helm3); do
+while true; do
+   pgrep microk8s.helm3
    echo
    sudo microk8s kubectl get po
    sleep 15
