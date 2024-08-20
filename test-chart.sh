a367bf9ed44ce979288352aa9d5daa3e47572b82
Maksym Kryukov <max.krukov89@gmail.com>
2024-08-20 19:27:38 +0300

    Update test-chart.sh

--- test-chart.sh
+++ test-chart.sh
@@ -1,22 +1,16 @@
+#!/bin/bash -e
 CHART_NAME=$1
 
-sudo microk8s helm3 upgrade -i --atomic $CHART_NAME helm-chart-sources/$CHART_NAME --set workload=Deployment --set kubePrometheusStack.enabled=true
+helm3() {
+   sudo microk8s helm3 upgrade -i --atomic $CHART_NAME helm-chart-sources/$CHART_NAME --set workload=Deployment --set kubePrometheusStack.enabled=true
+}
 
-export STATUS=$(sudo microk8s kubectl get po | grep $CHART_NAME | awk '{print $3}')
-if [ "$STATUS" == "" ]
-then
-  exit 1
-fi
+helm3 &
 
-while [ $STATUS == "ContainerCreating" ]
-do
-  export STATUS=$(sudo microk8s kubectl get po | grep $CHART_NAME | awk '{print $3}')
-  sleep 1
+while (pgrep microk8s.helm3); do
+   echo
+   sudo microk8s kubectl get po
+   sleep 15
+   echo
 done
 
-sudo microk8s kubectl get po
-
-if [ $STATUS != "Running" ]
-then
-  exit 1
-fi
